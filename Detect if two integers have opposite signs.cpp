ac66e8b53b49bffe24f9dba30fed26dfa08d0505
amrutapatil-63 <103416392+amrutapatil-63@users.noreply.github.com>
2022-04-11 15:25:36 +0530

    Create Detect if two integers have opposite signs.cpp

---
+++ Detect if two integers have opposite signs.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+bool oppositeSigns(int x, int y)
+{
+	return ((x ^ y) < 0);
+}
+
+int main()
+{
+	int x = 100, y = -100;
+	if (oppositeSigns(x, y) == true)
+	cout << "Signs are opposite";
+	else
+	cout << "Signs are not opposite";
+	return 0;
+}
