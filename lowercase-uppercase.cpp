ad29025feeef9d63e24354ffb242e7aa59db5b3d
amrutapatil-63 <103416392+amrutapatil-63@users.noreply.github.com>
2022-04-11 14:26:59 +0530

    Update lowercase-uppercase.cpp

--- lowercase-uppercase.cpp
+++ lowercase-uppercase.cpp
@@ -3,10 +3,8 @@ using namespace std;
   
 int main()
 {
-    // su is the string which is converted to uppercase
     string su = "Jatin Goyal";
-  
-    // using transform() function and ::toupper in STL
+
     transform(su.begin(), su.end(), su.begin(), ::toupper);
     cout << su << endl;
     return 0;
